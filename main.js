3252fefe30a3826129e52593eb1bd42998f22d68
Patadss <ajdinskiivan1@gmail.com>
2024-12-01 23:46:39 +0100

    fixed drawing added rotating and easy drop test

--- main.js
+++ main.js
@@ -19,33 +19,47 @@ function remove_event_listener(){
 }
 
 function drop(shape){
-    let children = game_canvas.children;
-
-    let interval_id = window.setInterval(() => drop_shape(shape,interval_id), 100);
+    let interval_id = window.setInterval(() => drop_shape(shape,interval_id), 200);
     //clearInterval(drop_speed);
 
+    let key_down = false;
     document.addEventListener("keydown", function key_handle(event){
-        console.log(event.key + " " + event.code);
-        if(event.key === "q"){
-            clearInterval(interval_id);
+        if(key_down){
+            return;
+        }
+        key_down = true;
+        if(event.key === "q"){//TODO:make virtual event not be "q"
             document.removeEventListener("keydown", key_handle);
         }
         if(event.key === 'd'){
             if(check_collision_right(shape) !== -1){
-                check_collision(shape,interval_id);
                 erase_previous_block(shape);
+                check_collision(shape,interval_id);
                 shape.j++;
                 draw_block_on_canvas(shape);
             }
         }
         if(event.key === 'a'){
             if(check_collision_left(shape) !== -1){
-                check_collision(shape,interval_id);
                 erase_previous_block(shape);
+                check_collision(shape,interval_id);
                 shape.j--;
                 draw_block_on_canvas(shape);
             }
         }
+        if(event.key === 'w'){//TODO:check collision before rotating for both w and s
+            erase_previous_block(shape);
+            shape = rotate_left(shape);
+            draw_block_on_canvas(shape);
+        }
+        if(event.key === 's'){
+            erase_previous_block(shape);
+            shape = rotate_right(shape);
+            draw_block_on_canvas(shape);
+        }
+    });
+    document.addEventListener("keyup", function key_handle(event){
+        key_down = false;
     });
 }
 
@@ -89,10 +103,12 @@ function check_collision_right(shape){
 }
 
 function check_collision(shape,interval_id){
+    //TODO:doesn't work for the first line returns -1(fix) then works good
     let children = game_canvas.children;
     if(shape.i + shape.shape.length > 19){
         clearInterval(interval_id);
         remove_event_listener();
+        return -1;
     }
     let i = shape.shape.length-1;
     for(let j = 0;j < shape.shape[i].length;j++){
@@ -106,6 +122,7 @@ function check_collision(shape,interval_id){
         if(children[ph].style.backgroundColor !== "white"){
             clearInterval(interval_id);
             remove_event_listener();
+            return -1;
         }
     }
 }
@@ -114,10 +131,10 @@ function erase_previous_block(shape){
     let children = game_canvas.children;
     for(let i = 0;i < shape.shape.length;i++){
         for(let j = 0;j < shape.shape[i].length;j++){
-            if((shape.i-1 + i) * 10 + shape.j + j < 0 || shape.shape[i][j] === undefined){
+            if((shape.i + i) * 10 + shape.j + j < 0 || shape.shape[i][j] === undefined){
                 continue;
             }
-            children[(shape.i-1 + i)*10+shape.j + j].style.backgroundColor = "white";
+            children[(shape.i + i)*10+shape.j + j].style.backgroundColor = "white";
         }
     }
 }
@@ -135,14 +152,65 @@ function draw_block_on_canvas(shape){
 }
 
 function drop_shape(shape,interval_id){
-    let children = game_canvas.children;
+    //let children = game_canvas.children;
 
-    check_collision(shape,interval_id);
+    if(check_collision(shape,interval_id) === -1){
+        return;
+    }
 
     erase_previous_block(shape);
 
-    draw_block_on_canvas(shape);
     shape.i++;
+    draw_block_on_canvas(shape);
+}
+
+function rotate_left(shape){
+    let n_shape = Object.create(shape);
+    let rows = shape.shape.length;
+    let col = shape.shape[0].length;
+    n_shape.shape = new Array(col);
+    for(let i = 0;i < n_shape.shape.length;i++){
+        n_shape.shape[i] = new Array(rows);
+        for(let j = 0;j < n_shape.shape[i].length;j++){
+            n_shape.shape[i][j] = 0;
+        }
+    }
+    for(let i = rows-1,ph = 0;i >= 0;i--){
+        let row = shape.shape[i];
+        for(let j = 0;j < row.length;j++){
+            n_shape.shape[j][ph] = row[j];
+        }
+        ph++;
+    }
+    return n_shape;
+}
+
+function rotate_right(shape){
+    let n_shape = Object.create(shape);
+    let rows = shape.shape.length;
+    let col = shape.shape[0].length;
+    n_shape.shape = new Array(col);
+    for(let i = 0;i < n_shape.shape.length;i++){
+        n_shape.shape[i] = new Array(rows);
+        for(let j = 0;j < n_shape.shape[i].length;j++){
+            n_shape.shape[i][j] = 0;
+        }
+    }
+    for(let i = 0,ph = 0;i < rows;i++){
+        let row = shape.shape[i];
+        for(let j = 0;j < row.length;j++){
+            n_shape.shape[j][ph] = row[j];
+        }
+        ph++;
+    }
+    return n_shape;
+}
+
+function btnDrop(){
+    let p = [i_block,t_block,j_block,l_block,o_block,s_block,l_block];
+    let b = document.getElementById("select").value;
+    remove_event_listener();
+    drop(Object.create(p[b]));
 }
 
 let i_block = {
@@ -159,7 +227,7 @@ let i_block = {
 
 let t_block = {
     shape:[
-        [undefined,1],
+        [undefined,1,undefined],
         [1,1,1]
     ],
     i:-1,
@@ -199,8 +267,8 @@ let z_block = {
 
 let l_block = {
     shape:[
-        [1],
-        [1],
+        [1,undefined],
+        [1,undefined],
         [1,1]
     ],
     i:-2,
